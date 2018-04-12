af3224e902c5de68e0d4870b99a105f60df85bec
Falcon Wong <wywfalcon@gmail.com>
2018-04-12 14:12:18 -0700

    please run playbooks from /vagrant/playbooks instead to make sure it is the most updated

--- provisions/mgmt.sh
+++ provisions/mgmt.sh
@@ -6,10 +6,6 @@ apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible
 
-# Copy files
-cp -a /vagrant/* /home/vagrant/
-chown -R vagrant:vagrant /home/vagrant
-
 # configure hosts file for our internal network defined by Vagrantfile
 cat >> /etc/hosts <<EOL
 
