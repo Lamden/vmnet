3a0c769f10aec879e620f68122a92583fd46f51e
Falcon Wong <wywfalcon@gmail.com>
2018-06-01 11:33:31 -0700

    new logger

--- vmnet/test/logger.py
+++ vmnet/test/logger.py
@@ -7,7 +7,8 @@ import coloredlogs
 def get_logger(name=''):
     filedir = "logs/{}".format(os.getenv('TEST_NAME', 'test'))
     filename = "{}/{}.log".format(filedir, os.getenv('HOSTNAME', name))
-    format = "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
+    # format = "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
+    format = '%(asctime)s.%(msecs)03d %(name)s[%(process)d][%(processName)s] %(levelname)-2s %(message)s'
 
     class ColoredFileHandler(logging.FileHandler):
         def __init__(self, *args, **kwargs):
