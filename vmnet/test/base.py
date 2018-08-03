154e218c094f99a5e6a0f325a432580668fe9319
Falcon Wong <wywfalcon@gmail.com>
2018-08-02 17:38:53 -0700

    now actual code is shown while decorators work the way it is expected to

--- vmnet/test/base.py
+++ vmnet/test/base.py
@@ -248,29 +248,60 @@ class BaseNetworkTestCase(unittest.TestCase, metaclass=BaseNetworkMeta):
             os.system('docker exec {} mkdir -p /app/{}'.format(node, join('profiles', cls.testname, node)))
 
     @classmethod
+    def get_fn_str(cls, fn, indent=0):
+        if not callable(fn): return ''
+        cv = inspect.getclosurevars(fn)
+        non_locals = dict(cv.nonlocals)
+        full_fn_str = cls._clear_indents(inspect.getsource(fn))
+        fn_str = indent * '    ' + full_fn_str[0] + '\n'
+        for m in non_locals:
+            if not callable(non_locals[m]):
+                fn_str += (indent+1) * '    ' + '{} = dill.loads({})'.format(m, dill.dumps(non_locals[m])) + '\n'
+        for m in non_locals:
+            if callable(non_locals[m]):
+                added_fn_str = cls.get_fn_str(non_locals[m], indent+1) + '\n'
+                added_fn_str = re.sub(r"def ([\w]+)\(", 'def {}('.format(m), added_fn_str)
+                fn_str += added_fn_str
+        for l in full_fn_str[1:]:
+            fn_str += indent * '    ' + l + '\n'
+        return fn_str
+
+    @classmethod
+    def _clear_indents(cls, s):
+        new_fn_str = ''
+        pattern = re.compile('^    ')
+        lines = s.split('\n')
+        new_lines = []
+        for line in lines:
+            if not pattern.match(line) and line != '':
+                return lines
+            new_lines.append(pattern.sub('', line))
+        return new_lines
+
+    @classmethod
     def execute_python(cls, node, fn, async=False, python_version=3.6, profiling=False):
-        # fn_str = inspect.getsource(fn)
-        dill_str = dill.dumps(fn)
+        fn_str = cls.get_fn_str(fn, indent=0)
         fname = 'tmp_exec_code_{}.py'.format(uuid.uuid4().hex)
         profname = join('profiles', cls.testname, node, fn.__name__)
         with open(fname, 'w+') as f:
-        #     new_fn_str = ''
-        #     pattern = re.compile('^    ')
-        #     lines = fn_str.split('\n')
-        #     for line in lines[1:]:
-        #         new_fn_str += pattern.sub('', line) + '\n'
-
             new_fn_str = """
 import dill
-fn = dill.loads({})
-fn()
-            """.format(dill_str)
+dill.detect.trace(True)
+{}
+{}()
+            """.format(fn_str, fn.__name__)
             if profiling:
                 new_fn_str = """
 import cProfile
 pr = cProfile.Profile()
 pr.enable()
+################################################################################
+#   Code Block BEGINS
+################################################################################
 {}
+################################################################################
+#   Code Block ENDS
+################################################################################
 pr.create_stats()
 pr.dump_stats('{}.stats')
                 """.format(new_fn_str, profname)
