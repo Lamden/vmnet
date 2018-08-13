23002aa0679f2fc7bc6e7df54a81fcdf3016c0c0
Falcon Wong <wywfalcon@gmail.com>
2018-08-13 13:13:56 -0700

    moving profiling to LProcess

--- vmnet/test/base.py
+++ vmnet/test/base.py
@@ -286,62 +286,17 @@ class BaseNetworkTestCase(unittest.TestCase, metaclass=BaseNetworkMeta):
     def execute_python(cls, node, fn, async=False, python_version=3.6, profiling=None):
         fn_str = cls.get_fn_str(fn, indent=0)
         dill_str = dill.dumps(fn)
-        fname = 'tmp_exec_code_{}_{}.py'.format(node, uuid.uuid4().hex)
+        fname = 'tmp_exec_code_{}.py'.format(uuid.uuid4().hex)
         profname = join('profiles', cls.testname, node, fn.__name__)
         with open(fname, 'w+') as f:
-            if profiling == 'n':
                 new_fn_str = """
-from vprof import runner, stats_server
-import cProfile, dill, json, pkg_resources
-dill.detect.trace(True)
-pr = cProfile.Profile()
-pr.enable()
-################################################################################
-#   Code Block BEGINS
-################################################################################
-{fn_str}
-################################################################################
-#   Code Block ENDS
-################################################################################
-g = dill.loads({globals})
-l = dill.loads({locals})
-cProfile.runctx("{fnname}()", g, l)
-pr.create_stats()
-pr.dump_stats('{profname}.stats')
-                """.format(fn_str=fn_str, fnname=fn.__name__,
-                    args=[], kwargs={}, profname=profname,
-                    profiling=profiling,
-                    globals=dill.dumps(globals()),
-                    locals=dill.dumps(locals()))
-            elif profiling:
-                new_fn_str = """
-from vprof import runner, stats_server
-import cProfile, dill, json, pkg_resources
-dill.detect.trace(True)
-################################################################################
-#   Code Block BEGINS
-################################################################################
-{fn_str}
-################################################################################
-#   Code Block ENDS
-################################################################################
-run_stats = runner.run_profilers(({fnname}, {args}, {kwargs}), '{profiling}')
-print('################################################################################')
-with open('{profname}.json', 'w+') as f:
-    run_stats['version'] = pkg_resources.get_distribution("vprof").version
-    f.write(json.dumps(run_stats))
-print('################################################################################')
-                """.format(fn_str=fn_str, fnname=fn.__name__, args=[], kwargs={}, profname=profname, profiling=profiling)
-            else:
-                new_fn_str = """
-import dill
+import dill, os
+os.environ["PROFILING"] = {profiling}
 {fn_str}
 {fnname}()
-                """.format(fn_str=fn_str, fnname=fn.__name__)
+                """.format(fn_str=fn_str, fnname=fn.__name__, profiling=profiling)
 
             f.write(new_fn_str)
-
-        cls.profiles.append(profname)
 
         os.system('docker cp {fname} {node}:/app/{fname}'.format(fname=fname, node=node))
         exc_str = 'docker exec {} /usr/bin/python{} {} {}'.format(
