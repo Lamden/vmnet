7b4fcef75f4f27ec9dfdea075a3ed31f7a5069f0
wywfalcon <wywfalcon@gmail.com>
2018-12-14 15:24:22 -0800

    try again son

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def package_data(path):
         all_files += [os.path.join(root, f) for f in files]
     return all_files
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 setup(
     name='vmnet',
